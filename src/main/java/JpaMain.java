6fc5015a3dd93c1da4e6d73dac3f61037cea7090
sunfkkc <rlcks77@nate.com>
2023-04-10 18:05:33 +0900

    refactor: 메인함수 구현

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -1,2 +1,22 @@
+import javax.persistence.*;
+
 public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("sample");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try{
+            tx.begin();
+            logic(em);
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    private static void logic(EntityManager em){}
 }
