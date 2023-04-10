3cf6c1039d0be318a28861b2b3fa6ad654bbfef1
sunfkkc <rlcks77@nate.com>
2023-04-10 18:04:58 +0900

    refactor: Member클래스에 Entity 애너테이션 추가

--- src/main/java/entity/Member.java
+++ src/main/java/entity/Member.java
@@ -1,12 +1,23 @@
 package entity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "MEMBER")
 public class Member {
+    @Id
+    @Column(name = "ID")
     private String id;
+    @Column(name = "NAME")
     private String username;
+
     private Integer age;
 
 }
