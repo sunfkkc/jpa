eb43b61d86e9f8e7ea831e298f4b375e63a9433f
sunfkkc <rlcks77@nate.com>
2023-04-10 18:33:29 +0900

    update: 멤버를 crud 하는 로직 구현

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -1,4 +1,8 @@
+import java.util.List;
+
 import javax.persistence.*;
+
+import entity.Member;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,5 +22,29 @@ public class JpaMain {
         emf.close();
     }
 
-    private static void logic(EntityManager em){}
+    private static void logic(EntityManager em){
+
+        String id = "id1";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("기찬");
+        member.setAge(26);
+
+        //등록
+        em.persist(member);
+
+        //수정
+        member.setAge(30);
+
+        //한건 조회
+        Member findMember = em.find(Member.class, id);
+        System.out.println(findMember);
+
+        //목록조회
+        List<Member> members = em.createQuery("select * from Member m", Member.class).getResultList();
+        System.out.println("member size: "+members.size());
+
+        //삭제
+        em.remove(member);
+    }
 }
