1dc5c74c562ed895212c9de3953693c3d3766d9e
sunfkkc <rlcks77@nate.com>
2023-04-10 17:35:39 +0900

    feat: Member 클래스 생성

---
+++ src/main/java/entity/Member.java
@@ -0,0 +1,12 @@
+package entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    private String id;
+    private String username;
+    private Integer age;
+
+}
