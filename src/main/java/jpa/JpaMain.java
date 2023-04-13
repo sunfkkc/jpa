7ccc1cec3a03074bc20b502dce1362394a182e42
sunfkkc <rlcks77@nate.com>
2023-04-14 01:34:49 +0900

    refactor: EntityManager, EntityTransaction를 인스턴스 변수에서 제거하고 SequenceBoard 테스팅

--- src/main/java/jpa/JpaMain.java
+++ src/main/java/jpa/JpaMain.java
@@ -4,18 +4,19 @@ package jpa;
 import javax.persistence.*;
 
 import jpa.entity.Member;
+import jpa.entity.SequenceBoard;
 
 public class JpaMain {
 
     private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("sample");
-    private static EntityManager em = emf.createEntityManager();
-    private static EntityTransaction tx = em.getTransaction();
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
 
         try{
             tx.begin();
-            logic();
+            logic(em);
             tx.commit();
         }catch (Exception e){
             tx.rollback();
@@ -25,11 +26,12 @@ public class JpaMain {
         emf.close();
     }
 
-    private static void logic(){
-        updateMember();
+    private static void logic(EntityManager em){
+        updateMember(em);
+        setSequenceId(em);
     }
 
-    private static void updateMember(){
+    private static void updateMember(EntityManager em){
         Member member = new Member();
         member.setUsername("기찬");
         member.setAge(26);
@@ -38,7 +40,11 @@ public class JpaMain {
         em.persist(member);
     }
 
-
+    private static void setSequenceId(EntityManager em){
+        SequenceBoard board = new SequenceBoard();
+        em.persist(board);
+        System.out.println(board); //id=1 출력
+    }
 
 
 }
