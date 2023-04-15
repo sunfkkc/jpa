e677271d3374b119abc86c563adcca25bf0f6886
sunfkkc <rlcks77@nate.com>
2023-04-16 02:21:00 +0900

    feat: 쇼핑몰 회원 엔티티 생성

---
+++ src/main/java/jpa/model/entity/Member.java
@@ -0,0 +1,29 @@
+package jpa.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+    @Builder
+    public Member(String name, String city, String street, String zipcode) {
+        this.name = name;
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
