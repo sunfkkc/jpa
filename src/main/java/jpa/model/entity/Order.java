3fdf8303e7a517625e727d07cf3f900fbadab7da
sunfkkc <rlcks77@nate.com>
2023-04-18 00:17:42 +0900

    refactor: 주문 상품 객체와 일대다 연관관계 생성

--- src/main/java/jpa/model/entity/Order.java
+++ src/main/java/jpa/model/entity/Order.java
@@ -1,6 +1,7 @@
 package jpa.model.entity;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Builder;
@@ -26,6 +28,9 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItem;
 
     private LocalDate orderDate;
     @Enumerated(EnumType.STRING)
